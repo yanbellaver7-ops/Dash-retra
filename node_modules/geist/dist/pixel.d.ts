2ea39b2305c3c13cb75a7dc662064179c9c7df2a
yanbellaver7-ops <yanbellaver7@gmail.com>
2026-03-28 18:33:42 -0300

    remove arquivos grandes

--- node_modules/geist/dist/pixel.d.ts
+++
@@ -1,7 +0,0 @@
-import { NextFontWithVariable } from "next/dist/compiled/@next/font";
-
-export declare const GeistPixelSquare: NextFontWithVariable;
-export declare const GeistPixelGrid: NextFontWithVariable;
-export declare const GeistPixelCircle: NextFontWithVariable;
-export declare const GeistPixelTriangle: NextFontWithVariable;
-export declare const GeistPixelLine: NextFontWithVariable;
--- node_modules/geist/dist/sans.js
+++
@@ -1,7 +0,0 @@
-import localFont from "next/font/local";
-
-export const GeistSans = localFont({
-  src: "./fonts/geist-sans/Geist-Variable.woff2",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
